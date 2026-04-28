7b316e6e6ba87932358d3b8f2b35688eea40969d
Cong2005 <thanhcong2005vm@gmail.com>
2026-04-28 20:52:12 +0700

    frontend- login/register

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { Provider as PaperProvider } from "react-native-paper";
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -14,6 +15,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <PaperProvider><Stack screenOptions={{ headerShown: false }} /></PaperProvider>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
